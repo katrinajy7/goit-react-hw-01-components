40ff6379fd069c9baca78c257883027da799db65
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-07-06 10:20:03 -0700

    DONE AFTER 4 DAYS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,9 @@ import Statistics from './Statistics/Statistics';
 import statisticalData from '../data/statistical-data.json';
 import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+
 
 export const App = () => {
   return (
@@ -21,7 +24,9 @@ export const App = () => {
 
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
 
     </div>
   );
 };
+
