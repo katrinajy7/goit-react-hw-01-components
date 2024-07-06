5686e8efbc17e262e0b3d567e2b0b1f0755f3960
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-07-05 17:53:02 -0700

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,15 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-     test
-    </div>
-  );
+  return  (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );    
 };
 
