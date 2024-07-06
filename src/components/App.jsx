e0bc127640a950dde9110d21ab4168aadbb0ba25
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-07-06 09:33:36 -0700

    upk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
+// App.jsx
+
 import React from 'react';
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
-import { Statistics } from './Statistics/Statistics';
+import Statistics from './Statistics/Statistics'; // Correct import for default export
 import statisticalData from '../data/statistical-data.json';
 
 export const App = () => {
@@ -14,7 +16,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-   <Statistics title="Upload stats" stats={statisticalData} />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,33 @@
+// Statistics.jsx
 
+import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-
-
-    const Statistics = ({ title, stats }) => (
-        <section className={css.statistics}>
-          <h2 className={css.title}>{title}</h2>
-      
-          <ul className={css.stats_list}>
-            {stats.map(stat => (
-              <li className={css.item} key={stats.id}>
-                <span className={css.label}>{stats.label}</span>
-                <span className={css.percentage}>{stats.percentage}%</span>
-              </li>
-            ))}
-          </ul>
-        </section>
-      );
-
-
-  Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.object,
-  };
-//   export default Statistics;
+const Statistics = ({ title, stats }) => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>{title}</h2>
   
+    <ul className={css.stats_list}>
+      {stats.map(stat => (
+        <li className={css.item} key={stat.id}>
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
 
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
-
-
-
-
-
+export default Statistics;
